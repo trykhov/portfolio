f853d32a91eaff9e636caf4c0fb8f95610825286
Try <khovct@gmail.com>
2019-08-25 17:06:38 -0700

    updated resume

--- front-end/src/components/Resume.js
+++ front-end/src/components/Resume.js
@@ -24,16 +24,34 @@ class Resume extends React.Component {
           </div>
           <div id="technical-skills" className="resume-section">
             <h2>Technical Skills</h2>
-            <p>JavaScript, React, Python, Java, CSS, HTML, Node.js, Flask, Git, SQL</p>
+            <p>JavaScript, Python, React-Native, HTML, CSS, SaSS, React.js, Node.js, Git, SQL, RESTful APIs</p>
           </div>
           <div id="experience" className="resume-section">
             <h2>Experience</h2>
             <div className="exp-box">
               <div className="first-line">
                 <div className="company">
+                  <a href={'https://www.trilogyed.com/'}>Trilogy Education</a>
+                </div>
+                <div className="duration">December 2018 - June 2019</div>
+              </div>
+              <div className="location">Berkeley, CA</div>
+              <div className="position">
+                <em>Data Analytics & Software Bootcamp Teaching Assistant</em>
+              </div>
+              <div className="role">
+                <ul>
+                  <li>Mentored class of ~30 students where 47% of graduates transitioned into data & software-related roles within 3 months of completing the bootcamp</li>
+                  <li>Accelerated student fluency in programming and data analytics using Python, JavaScript, and SQL upon completion of over 10 projects throughout a 6-month program</li>
+                </ul>
+              </div>
+            </div>
+            <div className="exp-box">
+              <div className="first-line">
+                <div className="company">
                   <a href={"http://g12transport.com"}>G-12 Transport</a>
                 </div>
-                <div className="duration">March 2019 - April 2019</div>
+                <div className="duration">March 2019 - May 2019</div>
               </div>
               <div className="location">Los Angeles, CA</div>
               <div className="position">
@@ -41,28 +59,9 @@ class Resume extends React.Component {
               </div>
               <div className="role">
                 <ul>
-                  <li>Created a full-stack website from scratch for a growing trucking company in order to establish an online presence.</li>
-                  <li>Implemented the site’s core features using ​React​, ​Node.js​ and Sendgrid API that allows potential clients to send service requests to the business owner.</li>
-                  <li>Built translation feature using ​Redux​ to dynamically convert all text on the site to multiple languages (i.e. English and Spanish).</li>
-                  <li>Developed autocomplete functionality for location inputs using Google Map API.</li>
-                </ul>
-              </div>
-            </div>
-            <div className="exp-box">
-              <div className="first-line">
-                <div className="company">
-                  <a href={'https://www.trilogyed.com/'}>Trilogy Education</a>
-                </div>
-                <div className="duration">December 2018 - Present</div>
-              </div>
-              <div className="location">Berkeley, CA</div>
-              <div className="position">
-                <em>Data Analytics & Software Teaching Assistant</em>
-              </div>
-              <div className="role">
-                <ul>
-                  <li>Trained students with little or no programming background to proficiency level in R, Python, SQL, HTML, CSS, and Javascript through course lessons and projects.</li>
-                  <li>Planned numerous projects for students to apply their skills, taking large datasets and inserting them into a database while also creating web-dashboards to showcase the data on the frontend</li>
+                  <li>Established a growing trucking company’s online presence through implementation of full-stack website developed using React.js, Node.js, and Express</li>
+                  <li>Increased client engagement by 23% after implementing Spanish translation feature using React-Redux</li>
+                  <li>Introduced easy navigation and communication features between clients and company using RESTful APIs such as Google Maps and Sendgrid</li>
                 </ul>
               </div>
             </div>
@@ -71,18 +70,18 @@ class Resume extends React.Component {
                 <div className="company">
                   <a href={'https://www.kiwicampus.com/'}>Kiwi Campus</a>
                 </div>
-                <div className="duration">June 2018 - December 2018</div>
+                <div className="duration">May 2018 - December 2018</div>
               </div>
               <div className="location">​Berkeley, CA</div>
               <div className="position">
-                <em>Software Engineering Intern</em>
+                <em>Contract Software Engineer</em>
               </div>
               <div className="role">
                 <ul>
-                  <li>Worked as a developer at Kiwi Campus, a Berkeley based startup focused on revolutionizing the food delivery industry with autonomous robots</li>
-                  <li>Developed automation scripts in ​Python​ to clean and analyze data gathered from the company’s mobile app and delivery robots</li>
-                  <li>Created an internal dashboard using ​Flask​ to visualize key company metrics</li>
-                  <li>Successfully cemented partnerships with other delivery services such as Doordash and Snackpass using data to show why robots are more efficient than human couriers in terms of cost and time</li>
+                  <li>Pioneered the first centralized data dashboard that kept track of KPIs and other performance metrics used by over 10 teams in the United States, Colombia, and China</li>
+                  <li>Succeeded in helping to close a $1.9M investment deal using data visualizations, generated using Python and Plotly.py, to show outstanding company performance and growth</li>
+                  <li>Reduced delivery times by 34% after Operations team redesigned its logistics system using insights from the organized data</li>
+                  <li>Produced and coordinated a growing team of data analysts and software engineers to maintain and innovate the logistical component of the Product team</li>
                 </ul>
               </div>
             </div>
